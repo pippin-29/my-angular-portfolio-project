7a942c219389828784f1b58eb5b45533617446aa
Daniel Haddington <operas.referee.0e@icloud.com>
2024-01-28 04:41:26 +1030

    ...

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './components/app.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+@ NgModule({
+
+declarations: [
+	AppComponent
+],
+
+imports: [
+	BrowserModule
+],
+
+bootstrap: [AppComponent]
+
+})
+
+export class AppModule { }
