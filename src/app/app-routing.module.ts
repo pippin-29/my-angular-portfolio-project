5863a9719fc5659a5919720e16acb426878e1fdf
Daniel Haddington <operas.referee.0e@icloud.com>
2024-01-29 13:41:19 +1030

    reformatted story

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { C_ProjectsComponent } from "./components/nav/c_projects/c_projects.component";
-import { FileViewComponent } from "./components/file-view/file-view.component";
 
 
 
 
 	const routes: Routes = [
-	{ path: 'components/file-view', component: FileViewComponent },
+
 	{ path: 'components/nav/c_projects', component: C_ProjectsComponent }
 ];
 
