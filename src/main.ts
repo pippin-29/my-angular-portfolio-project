10c83dbd17d6d0991424456e97f496fe2c0c2c8d
Daniel Haddington <operas.referee.0e@icloud.com>
2024-01-30 11:10:54 +1030

    documentation formatting

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
-/*	import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';:
+/*	
+->>> import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 This line imports the platformBrowserDynamic function from the 
-@angular/platform-browser-dynamic module.
-platformBrowserDynamic is responsible for dynamically bootstrapping 
-an Angular application in a browser environment.
-import { AppModule } from './app/app.module';:
+@angular/platform-browser-dynamic module. platformBrowserDynamic is responsible for 
+dynamically bootstrapping an Angular application in a browser environment.
+
+->>> import { AppModule } from './app/app.module';
 
 This line imports the root module of the Angular application, typically named AppModule.
 The root module (AppModule) is responsible for defining the structure of the application 
 and configuring the components, services, and other modules.
-platformBrowserDynamic().bootstrapModule(AppModule):
+
+->>> platformBrowserDynamic().bootstrapModule(AppModule)
 
 The platformBrowserDynamic function is called to obtain a platform for dynamically 
 bootstrapping the Angular application in a browser.
 The bootstrapModule method is then called with the root module (AppModule) as an argument. 
 This initiates the bootstrapping process for the Angular application.
-.catch((err) => console.error(err));:
+
+->>> -.catch((err) => console.error(err));
 
 The catch method is used to handle any errors that may occur during the bootstrapping 
 process.
