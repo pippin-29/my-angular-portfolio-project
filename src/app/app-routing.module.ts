58dee9e44921570765c519c932d72b29b0582702
Daniel Haddington <operas.referee.0e@icloud.com>
2024-01-31 09:37:49 +1030

    calculator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 /// ^^^ node_modules ^^^ ///
 
-
+import { CalculatorComponent } from "./components/nav/calculator/calculator.component";
 import { C_ProjectsComponent } from "./components/nav/c_projects/c_projects.component";
 /// ^^^ components for display within main column ^^^ ///
 
 
 const routes: Routes = [
+	{ path: 'components/nav/calculator', component: CalculatorComponent },
 	{ path: 'components/nav/c_projects', component: C_ProjectsComponent }
 ]; /// ^^^ definition of routes array, will contain the path to relevant component ^^^ ///
 
---
+++ src/app/components/nav/calculator/calculator.component.ts
@@ -0,0 +1,14 @@
+
+
+
+import { Component } from "@angular/core";
+
+@Component ({
+	selector:	'app-calculator',
+	templateUrl:	'calculator.component.html',
+	styleUrl:	'calculator.component.css'
+})
+
+export class CalculatorComponent {
+	
+}
