5cf6032a99f36f3a321ea5fae5f2403132001375
Daniel Haddington <operas.referee.0e@icloud.com>
2024-01-31 11:35:07 +1030

    styled side-column

--- src/app/components/side-column/side-column.component.ts
+++ src/app/components/side-column/side-column.component.ts
@@ -19,7 +19,7 @@ import { Component } from "@angular/core";
 })
 
 export class SideColumnComponent {
-		isSideColumnVisible = true;
+		isSideColumnVisible = false;
 
 		toggleSideColumn() {
 			this.isSideColumnVisible = !this.isSideColumnVisible;
